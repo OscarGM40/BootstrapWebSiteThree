d667bce40cf7d3f423af15ba51e3437f03d39566
Oscar <oscargm28@hotmail.com>
2020-09-21 17:11:42 +0200

    primer commit

---
+++ javascript/app.js
@@ -0,0 +1,19 @@
+
+ScrollReveal().reveal('#imgProduct',{
+    duration:2000,
+    origin:'right',
+    distance:'150px'
+})
+
+ScrollReveal().reveal('#header-left',{
+    duration:2000,
+    origin:'bottom',
+    distance:'150px'
+})
+
+
+ScrollReveal().reveal('.cont',{
+    duration:2000,
+    origin:'left',
+    distance:'150px'
+})
